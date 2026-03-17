dee23f9a447233df7b371907e7827a91fb678426
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-03-17 14:27:28 +0530

    Some changes

--- src/Js/FromServer/verticalV1/FormLoad/DOMContentLoaded/funcToRun.js
+++ src/Js/FromServer/verticalV1/FormLoad/DOMContentLoaded/funcToRun.js
@@ -1,12 +1,10 @@
 import { StartFunc as StartFuncAddListeners } from "./AddListeners/startFunc.js";
 import { StartFunc as StartFuncFetchAsGetForDatalist } from "./FetchAsGetForDatalist/entryFile.js";
-import { StartFunc as StartFuncFromFetchAsGet } from "./FetchAsGet/entryFile.js";
 
 const StartFunc = async () => {
     StartFuncAddListeners();
     StartFuncFetchAsGetForDatalist();
     LocalFunc1();
-    StartFuncFromFetchAsGet();
 
 
 };
